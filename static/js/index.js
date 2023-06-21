26f0399886575ed24a2b6a3a200d4bcf43384292
SHADOWING <lewandoski2010@gmail.com>
2023-06-21 23:18:04 +0300

    adding the js file

---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+// Start header part --------------------------------------------------------------------------
+let listIcon = document.querySelector(".list-icon");
+let medialistDiv = document.querySelector(".media-list");
+
+listIcon.onclick = () => {
+
+    // $(medialistDiv).slideToggle()
+
+    $(medialistDiv).toggle("right");
+}
+
+
+// End header part ----------------------------------------------------------------------------
